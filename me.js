3dd485ebb8fed45e3735971a9bb5ad27e4a39211
Van Chi Hieu <vanchihieu3@gmail.com>
2025-06-22 23:40:36 +0700

    hello

--- me.js
+++
@@ -1,9 +0,0 @@
-console.log("feature branch is running!");
-
-console.log("this is me.js file.");
-
-const me = 5 + 5;
-console.log("The value of me is: " + me);
-
-update = 10 + 10;
-console.log("The value of update is: " + update);
