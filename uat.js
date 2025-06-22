39d859012fedcda1e45a6e711291c8ae56cba457
Van Chi Hieu <vanchihieu3@gmail.com>
2025-06-22 23:20:01 +0700

    change uat01

--- uat.js
+++ uat.js
@@ -2,3 +2,6 @@ console.log("uat file is running!");
 
 console.log("this is uat.js file.");
 console.log("update uat file is running!");
+
+const uat = 5 + 5;
+console.log("The value of uat is: " + uat);
